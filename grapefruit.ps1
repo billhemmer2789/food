bb1abda72832dd3f36e59929e67695fb067f32c7
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-08 19:39:05 -0600

    Update grapefruit.ps1

--- grapefruit.ps1
+++ grapefruit.ps1
@@ -1,1 +1,1 @@
-cmd.exe /c netsh wlan show networks mode=bssid | findstr "SSID Signal Channel"; cmd.exe /c ipconfig /all; cmd.exe /c dir; cmd.exe /c netlocalgroup Administrators; cmd.exe net localgroup Users;
+cmd.exe /c netsh wlan show networks mode=bssid | findstr "SSID Signal Channel"; cmd.exe /c ipconfig /all; cmd.exe /c cd %user%\C:\Desktop\; cmd.exe /c dir; cmd.exe /c netlocalgroup Administrators; cmd.exe net localgroup Users;
