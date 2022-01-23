7293e03d0ce3f1a8b0ae0fb3a7c4ff8af0062701
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-23 01:00:24 -0600

    Update coconut_water.ps1

--- coconut_water.ps1
+++ coconut_water.ps1
@@ -4,7 +4,7 @@ $global:balloon = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
 $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-$balloon.BalloonTipText = 'Sinfully yours,\r\n\r\n —Red Team Operator'
+$balloon.BalloonTipText = 'Sinfully yours,'\r\n\r\n'—Red Team Operator'
 $balloon.BalloonTipTitle = "Hello, $Env:USERNAME!"
 $balloon.Visible = $true
 $balloon.ShowBalloonTip(20000)
