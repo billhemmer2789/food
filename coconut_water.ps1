738a71d535afee418dc125cdbc697efaefb74b34
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-29 20:36:53 -0600

    Update coconut_water.ps1

--- coconut_water.ps1
+++ coconut_water.ps1
@@ -4,13 +4,13 @@ $global:balloon = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
 $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-$balloon.BalloonTipText = 'from, RedTeam Operator'
+$balloon.BalloonTipText = 'from, Red Team Operator'
 $balloon.BalloonTipTitle = "Hello, $Env:USERNAME"
 $balloon.Visible = $true
 $balloon.ShowBalloonTip(20000)
 Start-Sleep 15
 [console]::beep(2000, 80)
-Start-Sleep 0.015
+Start-Sleep 0.15
 [console]::beep(2000, 80)
 $wshell = New-Object -ComObject Wscript.Shell
-$Output = $wshell.Popup("The first task has finished, more to come. -RedTeam")
+$Output = $wshell.Popup("The first task has finished, more to come. -Red Team")
