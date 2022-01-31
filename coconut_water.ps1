dfbef17a8bbac034b469455afc99bb88360134fd
billhemmer2789 <97331850+billhemmer2789@users.noreply.github.com>
2022-01-31 02:32:06 -0600

    Update coconut_water.ps1

--- coconut_water.ps1
+++ coconut_water.ps1
@@ -1,4 +1,4 @@
-[console]::beep(4000, 3000)
+#[console]::beep(4000, 3000)
 Add-Type -AssemblyName System.Windows.Forms
 $global:balloon = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
@@ -9,8 +9,8 @@ $balloon.BalloonTipTitle = "Hello, $Env:USERNAME"
 $balloon.Visible = $true
 $balloon.ShowBalloonTip(20000)
 Start-Sleep 15
-[console]::beep(2000, 80)
+#[console]::beep(2000, 80)
 Start-Sleep 0.15
-[console]::beep(2000, 80)
+#[console]::beep(2000, 80)
 $wshell = New-Object -ComObject Wscript.Shell
 $Output = $wshell.Popup("The first task has finished, more to come. -Red Team")
